da7f60b3e98638218b9c4bd0c22c0cb6b1541dbd
BuildTools <unconfigured@null.spigotmc.org>
2018-05-20 13:51:18 +0200

    Añadido el método de comprobar color y modificado el atributo de color

--- Ejercicio4POO/src/ejercicio4poo/Electrodomestico.java
+++ Ejercicio4POO/src/ejercicio4poo/Electrodomestico.java
@@ -10,42 +10,34 @@ package ejercicio4poo;
  * @author Pues4Aula4
  */
 public class Electrodomestico {
-    
-    public static enum Color
-    {
-        BLANCO,
-        NEGRO,
-        ROJO,
-        AZUL,
-        GRIS
-    }
-    
-    private static final double PRECIO_BASE=100;
-    private static final Color COLOR=Color.BLANCO;
-    private static final char CONSUMO='F';
-    private static final double PESO=5;
-    
-    
+
+ 
+
+    private static final double PRECIO_BASE = 100;
+    private static final String COLOR = "BLANCO";
+    private static final char CONSUMO = 'F';
+    private static final double PESO = 5;
+
     private double precio_base;
-    private Color color;
+    private String color;
     private char consumo;
-    private double peso; 
+    private double peso;
 
     public Electrodomestico() {
-        this.precio_base=PRECIO_BASE;
-        this.color=COLOR;
-        this.consumo=CONSUMO;
-        this.peso=PESO;
+        this.precio_base = PRECIO_BASE;
+        this.color = color;
+        this.consumo = CONSUMO;
+        this.peso = PESO;
     }
 
     public Electrodomestico(double precio_base, double peso) {
         this.precio_base = precio_base;
         this.peso = peso;
-        this.color=COLOR;
-        this.consumo=CONSUMO;
+        this.color = color;
+        this.consumo = CONSUMO;
     }
 
-    public Electrodomestico(double precio_base, Color color, char consumo, double peso) {
+    public Electrodomestico(double precio_base, String color, char consumo, double peso) {
         this.precio_base = precio_base;
         this.color = color;
         this.consumo = consumo;
@@ -56,7 +48,7 @@ public class Electrodomestico {
         return precio_base;
     }
 
-    public Color getColor() {
+    public String getColor() {
         return color;
     }
 
@@ -72,16 +64,20 @@ public class Electrodomestico {
     public String toString() {
         return "Electrodomestico{" + "precio_base=" + precio_base + ", color=" + color + ", consumo=" + consumo + ", peso=" + peso + '}';
     }
-    
-    public void comprobarConsumoEnergetico(char letra){
-        if (letra.equals('A')){
+
+    public void comprobarConsumoEnergetico(char letra) {
+        //if (letra.equals('A')) {
             //no deja comprobar que sea igual a A
+        //}
+    }
+
+    public void comprobarColor(String color) {
+        if (color.equals("BLANCO") || color.equals("NEGRO") || color.equals("ROJO") || color.equals("AZUL") || color.equals("GRIS")) {
+            this.color = color;
+        }
+        else{
+            this.color = COLOR;
         }
     }
-    
-    
-    
-    
-    
-    
+
 }
