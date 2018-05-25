8e9465f765b73073b94801e4f18c21acb03c1c80
Painiller97 <derteufel39@gmail.com>
2018-05-25 18:45:40 +0200

    Interfaz Prestable hecha.

---
+++ Ejercicio5POO/src/ejercicio5poo/Entregable.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio5poo;
+
+/**
+ *
+ * @author Pues4Aula4
+ */
+public interface Entregable {
+    public boolean entregar();
+    public boolean devolver();
+    public boolean isEntregado();
+    public void compareTo(Object a);
+}
