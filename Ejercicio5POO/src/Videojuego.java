8f296a524fe286db9377d6309f34d3d8c68d23aa
Painiller97 <derteufel39@gmail.com>
2018-05-25 18:35:38 +0200

    Creados los atributos y constantes de la clase Videojuego

---
+++ Ejercicio5POO/src/Videojuego.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Pues4Aula4
+ */
+public class Videojuego {
+    static final int HORAS = 10;
+    static final boolean ENTREGADO = false;
+    
+    private String titulo;
+    private int horas;
+    private boolean entregado;
+    private String genero;
+    private String compañia;
+}
