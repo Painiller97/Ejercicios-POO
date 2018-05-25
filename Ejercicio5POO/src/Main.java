24ee0a4440a9eb56d992cbaa168a97bc96f13309
Painiller97 <derteufel39@gmail.com>
2018-05-25 19:12:36 +0200

    Comenzada la clase Main

--- Ejercicio5POO/src/Main.java
+++ Ejercicio5POO/src/Main.java
@@ -15,7 +15,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Serie listaseries[] = new listaseries[4];
     }
     
 }
