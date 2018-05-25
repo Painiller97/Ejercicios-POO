5072e43cafa2a57c973e36d7ee717bb6b332b89f
Painiller97 <derteufel39@gmail.com>
2018-05-25 18:40:38 +0200

    Clase Videojuego terminada

--- Ejercicio5POO/src/Videojuego.java
+++ Ejercicio5POO/src/Videojuego.java
@@ -17,4 +17,61 @@ public class Videojuego {
     private boolean entregado;
     private String genero;
     private String compañia;
+
+    public Videojuego(String titulo, int horas, String genero, String compañia) {
+        this.titulo = titulo;
+        this.horas = horas;
+        this.entregado = ENTREGADO;
+        this.genero = genero;
+        this.compañia = compañia;
+    }
+
+    public Videojuego(String titulo, int horas) {
+        this.titulo = titulo;
+        this.horas = horas;
+        this.entregado = ENTREGADO;
+        this.genero = " Género no especificado ";
+        this.compañia = " Compañía no especificada ";
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCompañia() {
+        return compañia;
+    }
+
+    public void setCompañia(String compañia) {
+        this.compañia = compañia;
+    }
+
+    @Override
+    public String toString() {
+        return "Videojuego{" + "titulo=" + titulo + ", horas=" + horas + ", entregado=" + entregado + ", genero=" + genero + ", compa\u00f1ia=" + compañia + '}';
+    }
+
+    
+
 }
+
