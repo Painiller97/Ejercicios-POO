43fdcbad9886c1e66f5e116ca8f689c392d88a56
BuildTools <unconfigured@null.spigotmc.org>
2018-05-30 20:55:52 +0200

    Creada la clase Libro

---
+++ EjercicioPOO6/src/ejerciciopoo6/Libro.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciopoo6;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Libro {
+    private String ISBN;
+    private String titulo;
+    private String autor;
+    private int paginas;
+
+    public Libro(String ISBN, String titulo, String autor, int paginas) {
+        this.ISBN = ISBN;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = paginas;
+    }
+    
+    
+}
