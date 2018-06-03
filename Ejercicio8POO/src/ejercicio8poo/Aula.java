caf6768d9180e9a44dde3eb2d5610edd101625f3
BuildTools <derteufel39@gmail.com>
2018-06-03 23:24:22 +0200

    Creada clase Aula con sus Atributos y Constructor.

---
+++ Ejercicio8POO/src/ejercicio8poo/Aula.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio8poo;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Aula {
+    private int identificador;
+    private int maximo;
+    private String destinada;
+
+    public Aula(int identificador, int maximo, String destinada) {
+        this.identificador = identificador;
+        this.maximo = maximo;
+        this.destinada = destinada;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public int getMaximo() {
+        return maximo;
+    }
+
+    public void setMaximo(int maximo) {
+        this.maximo = maximo;
+    }
+
+    public String getDestinada() {
+        return destinada;
+    }
+
+    public void setDestinada(String destinada) {
+        this.destinada = destinada;
+    }
+    
+    
+}
