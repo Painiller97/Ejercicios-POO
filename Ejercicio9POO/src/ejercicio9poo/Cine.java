74c93c9b982f39fbeea4cade9a382bfcdd108b0e
Painiller97 <derteufel39@gmail.com>
2018-06-04 18:34:27 +0200

    Creadas las clases del ejercicio y declarada la matriz en Cine.

--- Ejercicio9POO/src/ejercicio9poo/Cine.java
+++ Ejercicio9POO/src/ejercicio9poo/Cine.java
@@ -10,8 +10,14 @@ package ejercicio9poo;
  * @author Pues4Aula4
  */
 public class Cine {
-   private String pelicula;
-   private double precioEntrada;
+
+    private String pelicula;
+    private double precioEntrada;
+    private String asientos[][] = new String[7][8];
+
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_BLACK = "\u001B[30m";
 
     public Cine(String pelicula, double precioEntrada) {
         this.pelicula = pelicula;
@@ -38,6 +44,23 @@ public class Cine {
     public String toString() {
         return "Cine{" + "pelicula=" + pelicula + ", precioEntrada=" + precioEntrada + '}';
     }
-   
-   String asientos[][] = new String[8][9];
+
+    public static void crearAsientos() {
+        String asientos[][] = new String[10][10];
+        String col = "ABCDEFGHI";
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 0; j < col.length(); j++) {
+                asientos[i][j] = i + "" + col.charAt(j);
+                if(i%3==0){
+                    System.out.print(Cine.ANSI_GREEN);
+                }else if(i%2==0){
+                    System.out.print(Cine.ANSI_RED);
+                }
+                System.out.print(asientos[i][j] + " ");
+                System.out.print(Cine.ANSI_BLACK);
+            }
+            System.out.println("");
+        }
+
+    }
 }
