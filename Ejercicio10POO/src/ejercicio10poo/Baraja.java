9c437d8bd624da6a7ff1d1ae876eb47a62a30fca
BuildTools <derteufel39@gmail.com>
2018-06-17 11:38:09 +0200

    Creados atributos de Baraja y los Metodos Sueltos.

---
+++ Ejercicio10POO/src/ejercicio10poo/Baraja.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio10poo;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Baraja {
+    public static final int CARTAS = 40;
+    
+    private Carta carta[];
+    private int cartaSiguiente;
+
+    public Baraja() {
+        this.carta = new Carta[CARTAS];
+        this.cartaSiguiente = 0;
+    }
+ 
+    
+    
+    
+}
--- Ejercicio10POO/src/ejercicio10poo/Carta.java
+++ Ejercicio10POO/src/ejercicio10poo/Carta.java
@@ -15,5 +15,32 @@ public class Carta {
     
     private int numero;
     private String palo;
+
+    public Carta(int numero, String palo) {
+        this.numero = numero;
+        this.palo = palo;
+    }
+
+    @Override
+    public String toString() {
+        return "Carta{" + "NUMEROS=" + NUMEROS + ", PALOS=" + PALOS + ", numero=" + numero + ", palo=" + palo + '}';
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getPalo() {
+        return palo;
+    }
+
+    public void setPalo(String palo) {
+        this.palo = palo;
+    }
+    
     
 }
