6e0540c7294108c218a9d929174f44f1b753e57c
Painiller97 <cr998m.m@gmail.com>
2018-06-29 18:36:14 +0200

    Creada la clase Productos

---
+++ EjercicioPOO14/src/ejerciciopoo14/Productos.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciopoo14;
+
+/**
+ *
+ * @author Alumno
+ */
+public abstract class Productos {
+    protected String nombre;
+    protected double precio;
+
+    public Productos(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Productos{" + "nombre=" + nombre + ", precio=" + precio + '}';
+    }
+    
+    
+    
+}
