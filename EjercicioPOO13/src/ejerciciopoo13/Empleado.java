6ae3ef42f7f110dcd9ac1769eb0229db9890fb46
Painiller97 <cr998m.m@gmail.com>
2018-06-29 18:15:51 +0200

    Creada la clase Empleado

---
+++ EjercicioPOO13/src/ejerciciopoo13/Empleado.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciopoo13;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Empleado {
+    public final double PLUS = 300;
+    
+    private String nombre;
+    private int edad;
+    private double salario;
+
+    public Empleado(String nombre, int edad, double salario) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salario = salario;
+    }
+
+    
+    
+}
