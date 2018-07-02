92cbaf118bb18bd1a2666204392f3528fefc82b5
Painiller97 <cr998m.m@gmail.com>
2018-07-02 18:13:21 +0200

    Creada la clase Opcion

---
+++ Ejercicio18POO/src/ejercicio18poo/Opcion.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio18poo;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Opcion {
+    
+    private String textoOpcion;
+    private boolean correcto;
+
+    public String getTextoOpcion() {
+        return textoOpcion;
+    }
+
+    public void setTextoOpcion(String textoOpcion) {
+        this.textoOpcion = textoOpcion;
+    }
+
+    public boolean isCorrecto() {
+        return correcto;
+    }
+
+    public void setCorrecto(boolean correcto) {
+        this.correcto = correcto;
+    }
+
+    @Override
+    public String toString() {
+        return "Opcion{" + "textoOpcion=" + textoOpcion + ", correcto=" + correcto + '}';
+    }
+    
+    
+}
