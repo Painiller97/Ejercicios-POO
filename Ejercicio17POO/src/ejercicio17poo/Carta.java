e7dfb466fbb7ccf5e75c326448347f1d5e87f1dd
Painiller97 <cr998m.m@gmail.com>
2018-07-02 17:52:19 +0200

    Completado

--- Ejercicio17POO/src/ejercicio17poo/Carta.java
+++ Ejercicio17POO/src/ejercicio17poo/Carta.java
@@ -10,20 +10,21 @@ package ejercicio17poo;
  * @author Daniel
  */
 public class Carta {
+
     public enum PalosBarEspañola {
-    OROS, COPAS, ESPADAS, BASTOS 
-}
-    
+        OROS, COPAS, ESPADAS, BASTOS
+    }
+
     public enum PalosBarFrancesa {
-    DIAMANTES, PICAS, TREBOLES, CORAZONES 
-}
-    
+        DIAMANTES, PICAS, TREBOLES, CORAZONES
+    }
+
     public static final int[] NUMEROS = {1, 2, 3, 4, 5, 6, 7, 10, 11, 12};
     public static final int[] NUMEROSJUGAR = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
     public static final int[] NUMEROSFRANCESA = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
     public static final String[] PALOSESPAÑOLA = {"Bastos", "Copas", "Espadas", "Oros"};
     public static final String[] PALOSFRANCESA = {"Diamantes", "Picas", "Corazones", "Treboles"};
-    
+
     private int numero;
     private String palo;
     private String paloFrancesa;
@@ -36,7 +37,40 @@ public class Carta {
 
     @Override
     public String toString() {
-        return "Carta{" + "numero=" + numero + ", palo=" + palo + '}';
+        if (palo.equals("Corazones") || palo.equals("Diamantes") || palo.equals("Picas") || palo.equals("Treboles")) {
+            if (numero == 11) {
+                return "Carta{" + "numero=" + "Jota" + ", palo=" + palo + '}';
+            }
+            else if (numero == 12) {
+                return "Carta{" + "numero=" + "Reina" + ", palo=" + palo + '}';
+            }
+            else if (numero == 13) {
+                return "Carta{" + "numero=" + "Rey" + ", palo=" + palo + '}';
+            }
+            else if (numero == 1) {
+                return "Carta{" + "numero=" + "As" + ", palo=" + palo + '}';
+            }
+            else{
+                return "Carta{" + "numero=" + numero + ", palo=" + palo + '}';
+            }
+      
+        } else{
+            if (numero == 11) {
+                return "Carta{" + "numero=" + "Sota" + ", palo=" + palo + '}';
+            }
+            else if (numero == 12) {
+                return "Carta{" + "numero=" + "Caballo" + ", palo=" + palo + '}';
+            }
+            else if (numero == 13) {
+                return "Carta{" + "numero=" + "Rey" + ", palo=" + palo + '}';
+            }
+            else if (numero == 1) {
+                return "Carta{" + "numero=" + "As" + ", palo=" + palo + '}';
+            }
+            else{
+                return "Carta{" + "numero=" + numero + ", palo=" + palo + '}';
+            }
+        } 
     }
 
     public int getNumero() {
@@ -62,8 +96,5 @@ public class Carta {
     public void setPaloFrancesa(String paloFrancesa) {
         this.paloFrancesa = paloFrancesa;
     }
-    
-    
-    
-    
+
 }
